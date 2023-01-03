2b337534cbf613231b425f783a31a1dbf6f7c724
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2023-01-03 00:14:44 +0000

    check

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { User } from '../../interfaces';
 import { sampleUserData } from '../../utils/sample-data';
 import Layout from '../../components/Layout';
-import List from '../index/List';
+import List from '../../components/List';
 
 type Props = {
   items: User[];
