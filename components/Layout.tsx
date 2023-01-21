84e6ba3f90fb98f320b45ea51764ccba676b5d47
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2023-01-21 18:47:39 +0000

    Change structure

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { ReactNode } from 'react';
-import Header from './Header/Header';
+import Header from './Layout/Header';
 
 type Props = {
   children?: ReactNode;
